567f08e5d23e248d183942b4999c9b7290432d0f
21pnuttman <144434700+21pnuttman@users.noreply.github.com>
2025-03-28 10:30:49 +0000

    Update Program.cs

--- ShapeAreaCalculatorTask/Program.cs
+++ ShapeAreaCalculatorTask/Program.cs
@@ -10,10 +10,18 @@
             // Rectangle
             // Pentagon
             // Octagon
-        }
-        static int areaOfRectangle()//Add the parameters
-        {
 
-        }
+            Console.WriteLine("What do you want to find the area of?:);
+            Console.WriteLine("1 - Circle");
+            Console.WriteLine("2 - Triangle");
+            Console.WriteLine("3 - Rectangle");
+            Console.WriteLine("4 - Pentagon");
+            Console.WriteLine("5 - Octagon");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                Console.writeLine("What is the radius of the circle?")
+                                      
+            }
     }
 }
